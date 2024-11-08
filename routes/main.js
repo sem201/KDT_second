029bc8874503158f2fd0e38bf4515124e1e05ad8
choiyen <yyy1459@naver.com>
2024-11-08 14:03:59 +0900

    ddff

--- routes/main.js
+++ routes/main.js
@@ -5,6 +5,8 @@ const controller = require("../controller/Cmain");
 router.get("/login", controller.login);
 router.get("/signup", controller.signup);
 
+router.get("/", controller.index);
+
 router.get("/home", controller.index);
 
 module.exports = router;
