4b941e99a20e0adb2f63f3f35815afb511cb8ff9
syc031023 <syc031023@naver.com>
2024-11-12 14:05:53 +0900

    profile 구현

--- static/profile.js
+++ static/profile.js
@@ -1,3 +1,36 @@
 function selectMoim(src) {
     console.log(src.className);
-  }
+}
+
+async function participatingMoim() {
+    const { data } = await axios({
+        method: "GET",
+        url: "/user/participating"
+    });
+
+    console.log(data);
+
+    const tbody = document.querySelector("tbody");
+
+    if (data) {
+        let row = ``;
+
+        for (let i = 0; i < data.lengh; i++) {
+            if (i % 2 === 0){ row += `<tr>`; }
+            row = `<td>
+                  <div class="content" onclick="location.href='/moim/moim_detail/${data[i].moim_id}'">
+                    <div class="title">${data[i].title}</div>
+                    <img src="${data[i].represent_img}" alt="" />
+                  </div>
+                </td>`;
+            if ((i + 1) % 2 === 0){row += `</tr>`;}
+        }
+        if (data.length % 2 != 0) {
+            row += `<td></td><tr>`;
+        }
+        console.log(row);
+        tbody.innerHTML = row;
+    }
+
+
+}
